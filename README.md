b87fe05d4f1e2128d5b23030b1562014c81a9ff6
blade254353074 <blade254353074@hotmail.com>
2017-04-01 23:45:39 +0800

    Init

---
+++ README.md
@@ -0,0 +1,46 @@
+# scrollto-element [![Standard - JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+A simple animated scroll to element function.
+
+* Using requestAnimationFrame
+* Only require [bezier-easing](https://github.com/gre/bezier-easing) and [raf](https://github.com/chrisdickinson/raf)
+
+## Install
+
+```bash
+npm install scrollto-element --save
+```
+
+## Usage
+
+```javascript
+const scrollToElement = require('scrollto-element')
+// or ES2015+
+import scrollToElement from 'scrollto-element'
+```
+
+## Example
+
+```javascript
+import scrollToElement from 'scrollto-element'
+
+scrollToElement('#J_ScrollToMe')
+scrollToElement('#J_ScrollToMe', 300)
+// or
+scrollToElement({
+  selector: '#J_ScrollToMe',
+  bezier: [0.19, 1, 0.22, 1],
+  duration: 3000,
+  then () {
+    console.log('Finished~')
+  }
+})
+```
+
+## See also
+
+* [Easing Function Cheat Sheet](http://easings.net/)
+
+## License
+
+MIT
---
+++ src/scrollto-element.js
@@ -0,0 +1,46 @@
+import BezierEasing from 'bezier-easing'
+import raf from 'raf'
+import scrollableParent from './scrollable-parent'
+
+const easeOutExpo = BezierEasing(0.19, 1, 0.22, 1)
+
+/**
+ * Animation scroll
+ * @param {string} selector DOM Selector
+ * @param {number} [during=100] (Optional) Scroll during
+ */
+function scrollTo (target, during = 20) {
+  if (!isElement(target)) throw new Error('The first argument must be HTMLElement.')
+
+  let start = 0
+  const parent = scrollableParent(target)
+  const currentScrollTop = parent.scrollTop
+  const parentOffsetTop = parent.offsetTop
+  let targetScrollTop
+  if (parent.nodeName === 'BODY') {
+    targetScrollTop = target.getBoundingClientRect().top +
+      (window.scrollY || window.pageYOffset || document.body.scrollLeft) -
+      parentOffsetTop
+  } else {
+    targetScrollTop = target.offsetTop - parentOffsetTop
+  }
+  const delta = targetScrollTop - currentScrollTop
+
+  function _run () {
+    start++
+    const offset = easeOutExpo(start / during) * delta
+    parent.scrollTop = currentScrollTop + offset
+    if (start < during) raf(_run)
+  }
+  _run()
+}
+
+function isElement (input) {
+  return input != null &&
+    typeof input === 'object' &&
+    input.nodeType === window.Node.ELEMENT_NODE &&
+    typeof input.style === 'object' &&
+    typeof input.ownerDocument === 'object'
+}
+
+export default scrollTo
